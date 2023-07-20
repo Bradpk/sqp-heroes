9f84c89ffabbfa8f996ed2943195100e13ca5acb
brad <brad.pk@live.com>
2023-07-20 22:29:13 +0000

    g

--- src/read.py
+++ src/read.py
@@ -2,11 +2,12 @@ from database.connection import execute_query, create_connection
 
 def read_hero():
     query = """
-        SELECT name FROM heroes
+        SELECT name, about_me, biography FROM heroes
     """ 
-    names = execute_query(query).fetchall()
-    for count, value in enumerate(names):
-        print(f"{count + 1}: {value[0]}")
+    hero_data = execute_query(query).fetchall()
+    for count, value in enumerate(hero_data):
+        name, about_me, biography = value
+        print(f"{count + 1}: Name: {name}\nAbout Me: {about_me}\nBiography: {biography}\n")
 
 
 #Add the interactive terminal stuff later. Just make sure the basic functions work first!
