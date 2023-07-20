e70697fe2ed6010ae70ccfaa1f3b8f086f75f353
brad <brad.pk@live.com>
2023-07-20 14:55:00 +0000

    g

--- src/create.py
+++ src/create.py
@@ -1,1 +1,10 @@
 from database.connection import execute_query, create_connection
+
+def create_hero(name, about_me, biography):
+    query = """
+        INSERT INTO heroes (name, about_me, biography)
+        VALUES (%s, %s, %s)
+        """
+    execute_query(query, (name, about_me, biography))
+
+create_hero("Roy", "Me Little Strong", "Grocery Manager")
