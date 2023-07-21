d1934066ba4d8b5d9bde575244936a748caebaf6
brad <brad.pk@live.com>
2023-07-21 13:11:06 +0000

    g

--- src/delete.py
+++ src/delete.py
@@ -3,8 +3,8 @@ from database.connection import execute_query, create_connection
 def delete_hero(): 
     hero_name = input("What is the name of the hero you want to delete? ")
 
-    query_check = "SELECT COUNT(*) FROM heroes WHERE name = %s"
-    result = execute_query(query_check, (hero_name,)).fetchone()
+    check = "SELECT COUNT(*) FROM heroes WHERE name = %s"
+    result = execute_query(check, (hero_name,)).fetchone()
 
     if result[0] == 0:
         print("Not a valid hero name.")
